7c73a50faf15d204d434a2d4739015e4e6f81096
otofune <otofune@otofune.me>
2022-05-09 04:06:18 +0900

    stop all fork services

--- variables.tf
+++ variables.tf
@@ -1,23 +1,4 @@
-variable "discord_apikey" {
-  type      = string
-  sensitive = true
-}
-
-variable "discord_public_key" {
-  type      = string
-  sensitive = true
-}
-
-variable "discord_application_id" {
-  type      = string
-  sensitive = true
-}
-
 variable "github_repository" {
   type    = string
   default = "t98s/minecraft-server"
-}
-
-variable "gcf_minecraft_starter_zip_filepath" {
-  type = string
 }
